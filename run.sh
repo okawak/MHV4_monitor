05a64aa4df0a42d678816072dae10ecbcba92a27
KodaiOkawa <okawa@cns.s.u-tokyo.ac.jp>
2024-01-09 20:42:55 +0900

    run parameter value

--- run.sh
+++ run.sh
@@ -5,6 +5,6 @@ port_rate="9600"
 sse_interval="1000" # ms
 voltage_step="5"    # 5 -> 0.5 V
 waiting_time="500"  # ms
-read_time="50"      # ms
+read_time="45"      # ms
 
 cargo run --release -- -p $port_name -r $port_rate -i $sse_interval -s $voltage_step -w $waiting_time -t $read_time
