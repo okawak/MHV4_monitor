c336961157dc21ada4a69f990d52f2d2b503e36c
okawak <kodak2022@outlook.jp>
2024-03-25 18:11:10 +0900

    mod: nodejs setting

--- public/src/app/tailwind-test/page.tsx
+++
@@ -1,5 +0,0 @@
-import "./style.css";
-
-export default function Page() {
-    return <h1 className="text-6xl font-bold text-blue-600">Tailwind Test</h1>;
-}
--- public/src/components/StatusCircle.tsx
+++
@@ -1,9 +0,0 @@
-interface StatusCircleProps {
-    status: "on" | "off";
-}
-
-const StatusCircle: React.FC<StatusCircleProps> = ({ status }) => {
-    return <div className={`status-circle ${status}`}> {/* スタイル適用 */}</div>;
-};
-
-export default StatusCircle;
