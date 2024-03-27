3df407ab5cb7138d41f3f2af76241c0565fce0a2
okawak <kodak2022@outlook.jp>
2024-03-27 18:11:36 +0900

    mod: run script

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@
 log_type="debug" # info, debug, trace
 
 # localhost server
-localhost="-l" # -l or blank
+localhost="" # -l or blank
 
 # for read error
 max_voltage="3000"
@@ -14,7 +14,7 @@ voltage_step="5"   # 5 -> 0.5 V
 waiting_time="500" # ms
 
 # kill the existing serial port process
-#pkill ${port_name}
+pkill mhv4_monitor
 
 # start the server
-RUST_LOG="${log_type}" cargo run --release --bin mhv4_monitor -- $localhost -p $port_name -r $port_rate -s $voltage_step -w $waiting_time -m $max_voltage
+RUST_LOG="${log_type}" cargo run --release --bin mhv4_monitor -- "$localhost" -p "$port_name" -r "$port_rate" -s "$voltage_step" -w "$waiting_time" -m "$max_voltage"
