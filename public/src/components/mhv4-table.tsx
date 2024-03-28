414639a374f7de7cdc28f80a8b624ce810d52434
okawak <kodak2022@outlook.jp>
2024-03-28 15:52:04 +0900

    mod: mhv4 table

--- public/src/components/mhv4-table.tsx
+++ public/src/components/mhv4-table.tsx
@@ -3,6 +3,8 @@
 import React from "react";
 import { useMHV4Data } from "@/contexts/MHV4Context";
 
+import { Switch } from "@/components/ui/switch";
+import { Input } from "@/components/ui/input";
 import {
   Table,
   TableBody,
@@ -14,11 +16,62 @@ import {
   TableRow,
 } from "@/components/ui/table";
 
+const processPolArray = (inputArray: boolean[]): string[] => {
+  return inputArray.map((value) => {
+    if (value) {
+      return "+";
+    } else {
+      return "-";
+    }
+  });
+};
+
+const processOnOffArray = (inputArray: boolean[]): string[] => {
+  return inputArray.map((value) => {
+    if (value) {
+      return "ON";
+    } else {
+      return "OFF";
+    }
+  });
+};
+
+
+const processVoltageArray = (inputArray: number[]): string[] => {
+  return inputArray.map((value) => {
+    if (value < -99999) {
+      return "read error!";
+    } else {
+      return (value * 0.1).toFixed(1).toString();
+    }
+  });
+};
+
+const processCurrentArray = (inputArray: number[]): string[] => {
+  return inputArray.map((value) => {
+    if (value < -99999) {
+      return "read error!";
+    } else {
+      return (value * 0.001).toFixed(3).toString();
+    }
+  });
+};
+
+
 const MHV4Table: React.FC = () => {
-  const { rcType, progressType, busArray, devArray, chArray, voltageArray, currentArray, isOnArray, isPositiveArray } = useMHV4Data();
+  const { progressType, busArray, devArray, chArray, voltageArray, currentArray, isOnArray, isPositiveArray } = useMHV4Data();
+  const pols = processPolArray(isPositiveArray);
+  const voltages = processVoltageArray(voltageArray);
+  const currents = processCurrentArray(currentArray);
+
+  let border_color = "border-green-500";
+  if (!progressType) {
+    border_color = "border-yellow-500";
+  }
+
   return (
-    <Table>
-      <TableHeader>
+    <Table className="border-solid text-center">
+      <TableHeader className="bg-blue-100 font-bold">
         <TableRow>
           <TableHead>bus</TableHead>
           <TableHead>dev</TableHead>
@@ -40,11 +93,11 @@ const MHV4Table: React.FC = () => {
             <TableCell>{devArray[index]}</TableCell>
             <TableCell>{chArray[index]}</TableCell>
             <TableCell>{isOnArray[index]}</TableCell>
-            <TableCell>{isOnArray[index]}</TableCell>
-            <TableCell>{isPositiveArray[index]}</TableCell>
-            <TableCell>{voltageArray[index]}</TableCell>
-            <TableCell>{voltageArray[index]}</TableCell>
-            <TableCell>{currentArray[index]}</TableCell>
+            <TableCell><Switch/></TableCell>
+            <TableCell>{pols[index]}</TableCell>
+            <TableCell><Input/></TableCell>
+            <TableCell>{voltages[index]}</TableCell>
+            <TableCell>{currents[index]}</TableCell>
             <TableCell>test</TableCell>
             <TableCell>test</TableCell>
           </TableRow>
